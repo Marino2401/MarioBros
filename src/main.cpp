47b85591a43e1a2bce46c0c9988653eaefa61d92
Marino2401 <159966620+Marino2401@users.noreply.github.com>
2024-10-22 22:37:49 +0000

    Clase 4

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,23 @@
 #include <Porcentaje.hpp>
 #include <list>
 
+using namespace std;
+
 int main (){
     std::list<Porcentaje> focos;
+
     Porcentaje foco1;
     Porcentaje foco2;
     Porcentaje foco3;
 
-focos.emplace_back(foco1);
-focos.emplace_back(foco2);
-focos.emplace_back(foco3);
+    focos.emplace_back(foco1);
+    focos.emplace_back(foco2);
+    focos.emplace_back(foco3);
 
-for (auto &&f : focos)
-{
-    f.Incrementar(10);
-}
-
+    for (auto &&f : focos)
+    {
+        f.Incrementar(10);
+    }
 
     foco1.Incrementar(10);
     foco1.Incrementar(10);
